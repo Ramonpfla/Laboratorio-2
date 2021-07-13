ad8c5d18d3f08af5e96d97d22eef7b30f66c0a9f
Ramonpfla <86633910+Ramonpfla@users.noreply.github.com>
2021-07-12 21:54:23 -0300

    Add files via upload

---
+++ Exerc_11/asm.s
@@ -0,0 +1,104 @@
+        PUBLIC  __iar_program_start
+        PUBLIC  __vector_table
+
+        SECTION .text:CODE:REORDER(1)
+        
+        ;; Keep vector table even if it's not referenced
+        REQUIRE __vector_table
+        
+        THUMB
+        
+__iar_program_start
+        
+main    
+  MOV R0, #3000
+  BL f32bits
+  B fim
+  
+f32bits
+  PUSH {R1, R2}
+  MOV R1, R0
+  CMP R1, #0
+  BEQ fatde0
+  SUB R1, #1
+loopf32b
+  CMP R1, #1
+  BEQ fatfim
+  MULS R0, R1
+  ADDS R2, R0, R0
+  BVS over
+  SUB R1, #1
+  B loopf32b
+over
+  MOV R0, #-1
+  POP {R1, R2}
+  BX LR
+fatfim
+  POP {R1, R2}
+  BX LR
+
+fatde0
+  MOV R0, #1
+  POP {R1, R2}
+  BX LR
+fim
+  B fim
+        ;; Forward declaration of sections.
+        SECTION CSTACK:DATA:NOROOT(3)
+        SECTION .intvec:CODE:NOROOT(2)
+        
+        DATA
+
+__vector_table
+        DCD     sfe(CSTACK)
+        DCD     __iar_program_start
+
+        DCD     NMI_Handler
+        DCD     HardFault_Handler
+        DCD     MemManage_Handler
+        DCD     BusFault_Handler
+        DCD     UsageFault_Handler
+        DCD     0
+        DCD     0
+        DCD     0
+        DCD     0
+        DCD     SVC_Handler
+        DCD     DebugMon_Handler
+        DCD     0
+        DCD     PendSV_Handler
+        DCD     SysTick_Handler
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Default interrupt handlers.
+;;
+
+        PUBWEAK NMI_Handler
+        PUBWEAK HardFault_Handler
+        PUBWEAK MemManage_Handler
+        PUBWEAK BusFault_Handler
+        PUBWEAK UsageFault_Handler
+        PUBWEAK SVC_Handler
+        PUBWEAK DebugMon_Handler
+        PUBWEAK PendSV_Handler
+        PUBWEAK SysTick_Handler
+
+        SECTION .text:CODE:REORDER:NOROOT(1)
+        THUMB
+
+NMI_Handler
+HardFault_Handler
+MemManage_Handler
+BusFault_Handler
+UsageFault_Handler
+SVC_Handler
+DebugMon_Handler
+PendSV_Handler
+SysTick_Handler
+Default_Handler
+__default_handler
+        CALL_GRAPH_ROOT __default_handler, "interrupt"
+        NOCALL __default_handler
+        B __default_handler
+
+        END
